b0b3d4688e01ff49dc6057413f3a74bd494caacc
jooyoung9939 <geore0309@korea.ac.kr>
2023-12-28 17:26:42 +0900

    공지사항 화면 수정

--- app/src/main/java/com/example/madcamp_week1_nsh_kjy_2/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1_nsh_kjy_2/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "my name is jooyoung"
     }
     val text: LiveData<String> = _text
 }
