eb4b5477f6290706609984ea941bcc989084bc60
jooyoung9939 <geore0309@korea.ac.kr>
2023-12-28 17:24:33 +0900

    홈 화면 수정

--- app/src/main/java/com/example/madcamp_week1_nsh_kjy_2/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1_nsh_kjy_2/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "my name is jooyoung"
     }
     val text: LiveData<String> = _text
 }
